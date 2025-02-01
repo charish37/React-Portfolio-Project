f8c48828d81b82f070398ced0e1dbf6cd3389c53
charish37 <charishmanadipalli77@gmail.com>
2025-02-01 15:05:13 -0500

    "adding routing for real-estate app"

--- real-estate-app/src/App.jsx
+++ real-estate-app/src/App.jsx
@@ -1,13 +1,22 @@
-
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import About from "./pages/About";
+import SignOut from "./pages/SignOut";
+import Profile from "./pages/Profile";
 
 function App() {
 
   return (
-    <>
-      <div className="text-red-500">
-        App
-      </div>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={Home} />
+        <Route path='/about' element={About} />
+        <Route path='/sign-in' element={SignIn} />
+        <Route path="/sign-out" element={SignOut} />
+        <Route path="/profile" element={Profile} />
+      </Routes>
+    </Router>
   )
 }
 
