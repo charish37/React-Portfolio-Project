3fa04294c8aef3994767324b5cea9ebe25724099
charish37 <charishmanadipalli77@gmail.com>
2025-02-07 22:47:08 -0500

    Complete Sign UP functionality

--- real-estate-project/real-estate-api/controllers/auth.control.js
+++ real-estate-project/real-estate-api/controllers/auth.control.js
@@ -2,10 +2,10 @@ import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
 
 export const signup = async(req,res,next) => {
+   try{
     const {username,email,password} = req.body;
     const hashPassword = bcrypt.hashSync(password,10);
     const newUser = new User({username,email,password: hashPassword});
-   try{
     await newUser.save()
     res.status(201).json("User created successfully")
    } catch(err){
--- real-estate-project/real-estate-app/src/pages/SignUp.jsx
+++ real-estate-project/real-estate-app/src/pages/SignUp.jsx
@@ -1,15 +1,56 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function SignUp() {
+
+  const [formData,setFormData] = useState({});
+  const [error,setError] = useState(null);
+  const [loading,setLoading] = useState(false);
+  const navigate = useNavigate();
+  
+  const handleChange = e => {
+     setFormData({
+      ...formData,
+      [e.target.id]: e.target.value
+     })
+  }
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    
+    try {
+      setLoading(true)
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData)
+      });
+      const data = await res.json();
+      setLoading(false);
+
+      if (!res.ok) {
+        throw new Error(data.message || 'Something went wrong!');
+      }
+      setError(null);
+      navigate('/sign-in')
+      console.log('Signup successful:', data);
+    } catch (err) {
+      setError(err.message);
+      setLoading(false);
+    }
+  }
+
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign UP</h1>
-      <form className='flex flex-col gap-4'>
-        <input type="text" placeholder='username' className='border p-3 rounded-lg' id='username' />
-        <input type="email" placeholder='email' className='border p-3 rounded-lg' id='email' />
-        <input type="password" placeholder='password' className='border p-3 rounded-lg' id='password' />
-        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Sign up</button>
+      <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+        <input type="text" placeholder='username' className='border p-3 rounded-lg' id='username' onChange={handleChange}/>
+        <input type="email" placeholder='email' className='border p-3 rounded-lg' id='email' onChange={handleChange}/>
+        <input type="password" placeholder='password' className='border p-3 rounded-lg' id='password' onChange={handleChange}/>
+        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>{loading ? 'Loading...' : 'Sign up'}</button>
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Have an account?</p>
@@ -17,6 +58,7 @@ export default function SignUp() {
           <span className='text-blue-700'>Sign in</span>
         </Link>
       </div>
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
   )
 }
